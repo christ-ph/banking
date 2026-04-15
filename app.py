4f45638c98da2a65a0e890f6971363031a1c33b5
christ-ph <dadjichrist5@gmail.com>
2026-04-15 20:16:49 +0100

    mise a jour du app.py pour la prodution

--- app.py
+++ app.py
@@ -29,6 +29,8 @@ def create_app():
     app.register_blueprint(api, url_prefix='/api/v1')
     return app
 
+# Création de l'instance pour Gunicorn (production)
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
